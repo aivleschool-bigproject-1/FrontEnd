6f06cd57936d9d5ee2150350cbd594190caf197b
ParkSunJin <sjp0402@gmail.com>
2024-07-20 18:03:36 +0900

    feat: 에디터 추가 (이미지 S3 업로드 연동)

---
+++ src/routes/Board/Editor.js
@@ -0,0 +1,88 @@
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+import {useCallback, useRef} from "react";
+import axios from "axios";
+
+const Editor = ({onChange, initialValue}) => {
+    const quillRef = useRef(null);
+
+    const uploadImage = useCallback(async (file) => {
+        const image = new FormData();
+        image.append('image', file);
+
+        const response = await axios.post('/s3-upload', image, {
+            headers: {
+                'Content-Type': 'multipart/form-data',
+                // TODO - user header 사용
+                'Authorization': 'JWTShield eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJCT0RBIiwiaWQiOjI2LCJleHAiOjE3MjE2Mzk4MTUsInVzZXJuYW1lIjoiYWRtaW4ifQ.mHIGpEpek13RiAE2Ia0-3o0MC0GsdpxSB4Umh5FkNcErTjk-DO1SPFzxGKMQ00rJzEFZZAqtczcGjpIzk3nvuA'
+            }
+        });
+
+        console.log(response);
+        if (response.status !== 200) {
+            throw new Error('image upload failed!');
+        }
+        return response.data;
+    }, []);
+
+    const imageHandler = useCallback(() => {
+        const input = document.createElement('input');
+        input.setAttribute('type', 'file');
+        input.setAttribute('accept', 'image/*');
+        input.click();
+
+        input.onchange = async () => {
+            const file = input.files[0];
+            console.log('file');
+            console.log(file);
+
+            try {
+                const imageUrl = await uploadImage(file);
+                const quill = quillRef.current.getEditor();
+                const range = quill.getSelection();
+                quill.insertEmbed(range.index, 'image', imageUrl);
+            } catch (error) {
+                console.error('Image upload failed:', error);
+            }
+        };
+    }, []);
+
+    const handleChange = useCallback((value) => {
+        onChange(value);
+    }, [onChange]);
+
+    return <div>
+        <ReactQuill
+            ref={quillRef}
+            value={initialValue}
+            modules={{
+                toolbar: {
+                    container: [
+                        [{header: [1, 2, false]}],
+                        ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+                        [
+                            {list: 'ordered'},
+                            {list: 'bullet'},
+                            {indent: '-1'},
+                            {indent: '+1'},
+                        ],
+                        ['image'],
+                        ['clean'],
+                    ],
+                    handlers: {
+                        image: imageHandler,
+                    },
+                },
+            }}
+            formats={[
+                'header',
+                'bold', 'italic', 'underline', 'strike', 'blockquote',
+                'list', 'bullet', 'indent',
+                'link', 'image'
+            ]}
+            onChange={handleChange}
+        />
+    </div>
+}
+
+export default Editor;
--- src/routes/Board/NoticeForm.js
+++ src/routes/Board/NoticeForm.js
@@ -1,65 +1,83 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
+import Editor from "./Editor";
+import axios from "axios";
+import {useNavigate} from "react-router-dom";
 
-const NoticeForm = ({ addNotice }) => {
-  const [noticeData, setNoticeData] = useState({
-    title: '',
-    content: '',
-    username: '', // username을 상태로 추가
-  });
-
-  useEffect(() => {
-    // 로컬 스토리지에서 username 가져오기
-    const storedUsername = localStorage.getItem('Username');
-    console.log('Stored username:', storedUsername); // 콘솔 로그 추가
-    if (storedUsername) {
-      setNoticeData(prevState => ({
-        ...prevState,
-        username: storedUsername,
-      }));
-    } else {
-      console.error('No username found in localStorage');
-    }
-  }, []);
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setNoticeData({
-      ...noticeData,
-      [name]: value,
+const NoticeForm = ({addNotice}) => {
+    const [noticeData, setNoticeData] = useState({
+        title: '',
+        content: '',
+        username: '',
     });
-  };
+    const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!noticeData.username) {
-      console.error('로그인 필요');
-      return;
-    }
-    console.log('Sending notice data:', noticeData); 
-    addNotice(noticeData);
-    setNoticeData({ title: '', content: '', username: noticeData.username });
-  };
+    useEffect(() => {
+        // 로컬 스토리지에서 username 가져오기
+        const storedUsername = localStorage.getItem('Username');
+        console.log('Stored username:', storedUsername); // 콘솔 로그 추가
+        if (storedUsername) {
+            setNoticeData(prevState => ({
+                ...prevState,
+                username: storedUsername,
+            }));
+        } else {
+            console.error('No username found in localStorage');
+        }
+    }, []);
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        name="title"
-        value={noticeData.title}
-        onChange={handleChange}
-        placeholder="제목"
-        required
-      />
-      <textarea
-        name="content"
-        value={noticeData.content}
-        onChange={handleChange}
-        placeholder="내용"
-        required
-      ></textarea>
-      <button type="submit">등록하기</button>
-    </form>
-  );
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setNoticeData({
+            ...noticeData,
+            [name]: value,
+        });
+    };
+
+    const handleSubmit = useCallback(async (e) => {
+        e.preventDefault();
+        if (!noticeData.username) {
+            console.error('로그인 필요');
+            return;
+        }
+        console.log('Sending notice data:', noticeData);
+        const response = await axios.post("/post", noticeData, {
+            headers: {
+                // TODO - 유저 인증 토큰을 전달
+                'Authorization': 'JWTShield eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJCT0RBIiwiaWQiOjI2LCJleHAiOjE3MjE2Mzk4MTUsInVzZXJuYW1lIjoiYWRtaW4ifQ.mHIGpEpek13RiAE2Ia0-3o0MC0GsdpxSB4Umh5FkNcErTjk-DO1SPFzxGKMQ00rJzEFZZAqtczcGjpIzk3nvuA'
+            }
+        })
+
+        if (response.status !== 201) {
+            throw new Error("post creeat failed!")
+        }
+
+        setNoticeData({title: '', content: '', username: noticeData.username});
+        navigate('/boards'); // TODO - 게시글로 이동
+    }, [noticeData]);
+
+    useEffect(() => {
+        console.log("changed");
+        console.log(noticeData.content)
+    }, [noticeData])
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                name="title"
+                value={noticeData.title}
+                onChange={handleChange}
+                placeholder="제목"
+                required
+            />
+
+            <Editor onChange={(value) => handleChange({
+                target: {name: "content", value: value}
+            })}
+                    initialValue={noticeData.content}/>
+            <button type="submit">등록하기</button>
+        </form>
+    );
 };
 
 export default NoticeForm;
