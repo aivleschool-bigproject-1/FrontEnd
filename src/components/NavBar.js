e4aeafcb54fa5e13790a55ca05df73c294e1b7f8
sunjin <sjp0402@gmail.com>
2024-07-21 16:11:35 +0900

    feat: admin 유저면 admin 페이지 이동 버튼 생성

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,39 @@
-import React, { useContext } from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 import { AuthContext } from '../Context/AuthContext';
 import './NavBar.css';
 
 const Navbar = () => {
     const { isLoggedIn, logout } = useContext(AuthContext);
     const navigate = useNavigate();
+    const [user, setUser] = useState(null);
+    // const isAdmin = user.Role;
 
     const handleLogout = () => {
         localStorage.removeItem('Username');
         logout();
         navigate('/');
     };
+
+    useEffect(() => {
+        const fetchUser = async () => {
+            const token = localStorage.getItem('Authorization');
+            try {
+                const response = await axios.get('/user', {
+                    headers: {
+                        'Authorization': `${token}`
+                    }
+                });
+                setUser(response.data);
+                console.log(response.data);
+            } catch (error) {
+                console.error('Error fetching user data:', error);
+            }
+        };
+
+        fetchUser();
+    }, []);
 
     return (
         <nav className="navbar">
@@ -27,8 +49,12 @@ const Navbar = () => {
                 <Link to="/boards">게시판</Link>
             </div>
             <div className="navbar-actions">
+                
                 {isLoggedIn ? (
                     <>
+                        {user && user.role === 'ROLE_ADMIN' && (
+                            <Link to="/admin" className="navbar-admin">Admin</Link>
+                        )}
                         <button onClick={handleLogout} className="navbar-logout">Logout</button>
                         <Link to="/profile" className="navbar-profile">My Profile</Link>
                     </>
