3a4b9d4987569741b8c4c64aaecbb01d904caf3b
sunjin <sjp0402@gmail.com>
2024-07-22 02:18:17 +0900

    logout

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,6 +11,7 @@ const Navbar = () => {
 
     const handleLogout = () => {
         localStorage.removeItem('Username');
+        localStorage.removeItem('Authorization');
         logout();
         navigate('/');
     };
